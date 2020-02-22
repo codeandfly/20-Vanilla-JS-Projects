9f462aaa43dab57bb314b156a3eb2fcbc3628459
Louis <louis@Louiss-Mac-mini.local>
2020-02-21 22:17:48 -0500

    Added LS and PopulateUI

--- Movie Seat Booking/script.js
+++ Movie Seat Booking/script.js
@@ -6,21 +6,55 @@ const movieSelect = document.getElementById('movie');
 
 let ticketPrice = +movieSelect.value;
 
+populateUI();
+
+// Save selected movie index and price
+function setMovieData(movieIndex, moviePrice) {
+  localStorage.setItem('selectedMovieIndex', movieIndex);
+  localStorage.setItem('selectedMoviePrice', moviePrice);
+}
+
 // Update total and count
 function updateSelectedCount() {
   const selectedSeats = document.querySelectorAll('.row .seat.selected');
 
+  const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
+
+  localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+
   const selectedSeatCount = selectedSeats.length;
-  
+
   count.innerText = selectedSeatCount;
   total.innerText = selectedSeatCount * ticketPrice;
+}
+
+// Get data from LS and populate UI
+function populateUI() {
+  const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+
+  // loop through selectedSeats
+  // Highlight selectedSeats if there are any
+  if (selectedSeats !== null && selectedSeats.length > 0) {
+    seats.forEach((seat, i) => {
+      if (selectedSeats.indexOf(i) > -1) {
+        seat.classList.add('selected');
+      }
+    });
+  }
+
+  const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+
+  if (selectedMovieIndex !== null) {
+    movieSelect.selectedIndex = selectedMovieIndex;
+  }
 }
 
 // Movie select event
 movieSelect.addEventListener('change', e => {
   ticketPrice = +e.target.value;
+  setMovieData(e.target.selectedIndex, e.target.value);
   updateSelectedCount();
-})
+});
 
 // Seat click event
 container.addEventListener('click', e => {
@@ -33,3 +67,6 @@ container.addEventListener('click', e => {
     updateSelectedCount();
   }
 });
+
+// Initial count and total set
+updateSelectedCount();
